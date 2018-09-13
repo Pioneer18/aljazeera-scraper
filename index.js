b7c4620e3b38ed0b6952bfa7be54b084a34caa91
Jonathan <sellsj14@gmail.com>
2018-09-13 00:17:04 -0400

    updated all the link grabs

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ app.get("/", function(req, res) {
         // then save the values for any "href" attributes that the child elements may have
         var title = $(element).find("div.top-feature-overlay-cont").find("a").find("h2.top-sec-title").text();
         //find the article description
-        var link = $(element).find("div.top-feature-overlay-cont").children("a").children("a").attr("href");
+        var link = "https://www.aljazeera.com" + $(element).find("div.top-feature-overlay-cont").children("a").children("a").attr("href");
   
         var desc = $(element).find("div.top-feature-overlay-cont").find("p.top-sec-desc").text();
   
@@ -115,8 +115,8 @@ app.get("/", function(req, res) {
   
         // In the currently selected element, look at its child elements (i.e., its a-tags),
         // then save the values for any "href" attributes that the child elements may have
-        var title = $(element).find("div.topFeature-sblock-wr").find("a").find("h2").text();
-        var link = $(element).find("div.topFeature-sblock-wr").children("a").attr("href");
+        var title = $(element).find("div.topFeature-sblock-wr").children("a").children("h2").text();
+        var link = "https://www.aljazeera.com" + $(element).find("div.topFeature-sblock-wr").children("a").children("a").attr("href");
         //find the article description
         //var img_url = $(element).find("div.topFeature-sblock-wr").find("a").children("img").eq(2).attr('src');
   
@@ -138,12 +138,12 @@ app.get("/", function(req, res) {
   
         // Save the text of the element in a "title" variable
         var topic = $(element).children("p.topics-sec-item-label").children("a").text();
-        var time = $(element).children("p.topics-sec-item-label").children("time").text();
+        //var time = $(element).children("p.topics-sec-item-label").children("time").text();
   
         // In the currently selected element, look at its child elements (i.e., its a-tags),
         // then save the values for any "href" attributes that the child elements may have
         var title = $(element).children("a").children("h2").text();
-        var link = $(element).children("a").attr("href");
+        var link = "https://www.aljazeera.com" + $(element).children("a").children("a").attr("href");
         //find the article description
         //var img_url = $(element).find("div.topFeature-sblock-wr").find("a").children("img").eq(2).attr('src');
         var desc =$(element).children("p.topics-sec-item-p").text();
