bd666b451a0b00813dea9d7d49a86011a6d85144
sellsj14 <sellsj14@gmail.com>
2018-12-27 00:40:35 -0500

    working on the index.js javascript

--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -25,32 +25,36 @@ $(document).ready(function () {
     });
   }
 
+
+  /*this feels like overkill, should be able to pass the array to the index and helpers do the rest */
   const renderArticles = (articles) => {
     //this function relys on JQuery to dynamically render bootstrap cards to the page containing the articles
     //the initPage function passes all the unsaved articles in the db into the articleCards array
     const articleCards = [];
     // each article JSON object is passed to the createCard function
     for (const i = 0; i < articles.length; i++) {
-      articleCards.push(createCard(articles[i]));
+      //articleCards.push(createCard(articles[i]));
+      articleCards.push(articles[i]);
     }
     //append the array of articles to the articles container
     //the #each helper will neatly render each card to the page
     articleContainer.append(articleCards);
   }
 
+  /*
   const createCard = (article) => {
   // constrcut article card from article json object
     const card = $("<div class='card'>");
     const cardHeader = $("<div class='card-header'>").append(
       $("<h3>").append(
-        $("<a class='article-link' target='_blank' rel='noopener noreferrer'>")
+        $("<a class='link' target='_blank' rel='link'>")
           .attr("href", article.link)
-          .text(article.article),
+          .text(article.topic),
         $("<a class='btn btn-success save'>Save Article</a>")
       )
     );
 
-    const cardBody = $("<div class='card-body'>").text(article.summary);
+    const cardBody = $("<div class='card-body'>").text(article.desc);
 
     card.append(cardHeader, cardBody);
     // We attach the article's id to the jQuery element
@@ -59,14 +63,14 @@ $(document).ready(function () {
     // We return the constructed card jQuery element
     return card;
   }
+  */
 
   const renderEmpty = () => {
-    // This function renders some HTML to the page explaining we don't have any articles to view
-    // Using a joined array of HTML string data because it's easier to read/change than a concatenated string
+    //Render this HTML to the page when there aren't any articles to display
     const emptyAlert = $(
       [
         "<div class='alert alert-warning text-center'>",
-        "<h4>Uh Oh. Looks like we don't have any new articles.</h4>",
+        "<h4>There aren't any new articles available at the momemnt.</h4>",
         "</div>",
         "<div class='card'>",
         "<div class='card-header text-center'>",
@@ -79,16 +83,15 @@ $(document).ready(function () {
         "</div>"
       ].join("")
     );
-    // Appending this data to the page
+    // Appending the html to the page
     articleContainer.append(emptyAlert);
   }
 
+  //event handler called when .btn.save class is clicked
   const handleArticleSave = () => {
-    // This function is triggered when the user wants to save an article
-    // When we rendered the article initially, we attached a javascript object containing the headline id
-    // to the element using the .data method. Here we retrieve that.
+    //grab the _id of this article (stored in the data-_id attribute)
     const articleToSave = $(this)
-      .parents(".card")
+      .parents(".card") 
       .data();
 
     // Remove card from page
@@ -96,8 +99,10 @@ $(document).ready(function () {
       .parents(".card")
       .remove();
 
+    //change this articles saved boolean to true
     articleToSave.saved = true;
-    // Using a patch method to be semantic since this is an update to an existing record in our collection
+    
+    //now update the db 
     $.ajax({
       method: "PUT",
       url: "/api/articles/" + articleToSave._id,
@@ -108,20 +113,22 @@ $(document).ready(function () {
         // Run the initPage function again. This will reload the entire list of articles
         initPage();
       }
-    });
+    }).catch((err) =>{
+      //log error with morgan
+      console.log(err);
+    })
   }
 
+  //event handler called when scrape-new class is clicked
   const handleArticleScrape = () => {
-    // This function handles the user clicking any "scrape new article" buttons
-    $.get("/api/fetch").then(function (data) {
-      // If we are able to successfully scrape the NYTIMES and compare the articles to those
-      // already in our collection, re render the articles on the page
-      // and let the user know how many unique articles we were able to save
+    //call on the axios route to scrape for articles (with the scrape Script)
+    $.get("/api/axios").then(function (data) {
+      //now reload the page with the newly scraped articles
       initPage();
-      bootbox.alert($("<h3 class='text-center m-top-80'>").text(data.message));
     });
   }
 
+  //event handler called when clear class clicked on
   const handleArticleClear = () => {
     $.get("api/clear").then(function () {
       articleContainer.empty();
